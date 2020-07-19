326bd7cc005d2015ddfb78f716bff4951e39fc65
Clarenz James Caba <clarenz.caba@gmail.com>
2020-07-19 15:30:19 -0400

    Add basic variables being handled

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Variable that keeps track of the total count
+// Starting point is zero (0)
+var totalCount = 0;
+
+/*
+    Variable that keeps track of the set maximum count
+    TODO: Get variable from page form
+*/
+var maxCount = 0;
+
+// Handle +1 button, add 1
+
+// Handle +2 button, add 2
+
+// Handle +3 button, add 3
+
+// Handle +4 button, add 4
+
+// Handle +5 button, add 5
+
+// Handle -1 button, subtract 1
+
+// Handle -2 button, subtract 2
+
+// Handle -3 button, subtract 3
+
+// Handle -4 button, subtract 4
+
+// Handle -5 button, subtract 5
