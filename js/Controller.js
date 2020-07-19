6eae264200543b543d6fb99043e30094ef142b19
Clarenz James Caba <clarenz.caba@gmail.com>
2020-07-19 19:36:12 -0400

    Rename controller to Controller

---
+++ js/Controller.js
@@ -0,0 +1,19 @@
+// Handle +1 button, add 1
+
+// Handle +2 button, add 2
+
+// Handle +3 button, add 3
+
+// Handle +4 button, add 4
+
+// Handle +5 button, add 5
+
+// Handle -1 button, subtract 1
+
+// Handle -2 button, subtract 2
+
+// Handle -3 button, subtract 3
+
+// Handle -4 button, subtract 4
+
+// Handle -5 button, subtract 5
